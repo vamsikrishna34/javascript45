17e4f86695e866e5b5744b9e04ccc21caa80c0b9
vamsikrishna34 <nvamsi.krishna2001@gmail.com>
2022-01-27 23:40:54 +0530

    did some practice

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 //document.getElementById("Count").innerText=5
 
-let count=5+7
+// let firstbatch=5
+// let secondbatch=7
 
-console.log(count)
+// let count= firstbatch +secondbatch
+
+// console.log(count)
+
+
+// let myage=21
+// let dogratio=7
+
+// let mydogage=myage * dogratio
+
+// console.log(mydogage)
+
+ //let count= 5
+
+// // count + 1
+//count = count + 1 // 5+1 =6
+
+// console.log(count)
+
+// let bonuspoints=50
+
+// console.log(bonuspoints)
+// bonuspoints=bonuspoints +50
+
+// bonuspoints= bonuspoints-75
+// console.log(bonuspoints)
+
+// bonuspoints=bonuspoints+45
+// console.log(bonuspoints)
 
